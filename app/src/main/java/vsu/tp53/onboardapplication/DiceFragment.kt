bd8691e69b499c196c15d23a1e8027a11679c09b
Владислав <yefremov.vlad@gmail.com>
2023-04-22 21:37:09 +0300

    ORG-10: Optimized imports

--- app/src/main/java/vsu/tp53/onboardapplication/DiceFragment.kt
+++ app/src/main/java/vsu/tp53/onboardapplication/DiceFragment.kt
@@ -1,10 +1,10 @@
 package vsu.tp53.onboardapplication
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import vsu.tp53.onboardapplication.databinding.FragmentDiceBinding
 
 /**
