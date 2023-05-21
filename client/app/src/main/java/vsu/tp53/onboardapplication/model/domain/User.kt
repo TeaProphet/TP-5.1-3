491bf04f8b9d7f55cb882acb0c49eeaf9303aaac
CezzarJ3 <silenia2014@gmail.com>
2023-05-21 16:51:26 +0300

    ORG-16: User registration and authorization dtos created, unit test for user reg/auth serialize/deserialize/map, gradle dependencies for test updated

---
+++ client/app/src/main/java/vsu/tp53/onboardapplication/model/domain/User.kt
@@ -0,0 +1,20 @@
+package vsu.tp53.onboardapplication.model.domain
+
+import kotlinx.serialization.Serializable
+import vsu.tp53.onboardapplication.model.entity.UserRegAuthorizePost
+import vsu.tp53.onboardapplication.model.entity.UserTokenPost
+
+@Serializable
+data class User(
+    var login: String,
+    var password: String
+) {
+    fun mapToEntity() = UserRegAuthorizePost(login, password)
+}
+
+@Serializable
+data class Token(
+    var idToken: String
+) {
+    fun mapToEntity() = UserTokenPost(idToken)
+}
