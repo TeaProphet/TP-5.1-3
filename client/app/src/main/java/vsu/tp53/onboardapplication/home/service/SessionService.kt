f443075da90fe9c37b4b976c8f7176cd64053879
Владислав <yefremov.vlad@gmail.com>
2023-06-06 12:39:57 +0300

    ORG-17: Added SessionService.

--- client/app/src/main/java/vsu/tp53/onboardapplication/home/service/SessionService.kt
+++ client/app/src/main/java/vsu/tp53/onboardapplication/home/service/SessionService.kt
@@ -1,18 +1,29 @@
 package vsu.tp53.onboardapplication.home.service
 
+import android.content.ContentValues
 import android.content.Context
 import android.os.StrictMode
 import android.util.Log
+import com.fasterxml.jackson.core.JsonGenerator
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import org.springframework.http.ResponseEntity
 import org.springframework.http.converter.json.GsonHttpMessageConverter
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
 import org.springframework.http.converter.json.MappingJacksonHttpMessageConverter
 import org.springframework.web.client.RestTemplate
 import vsu.tp53.onboardapplication.model.entity.SessionBody
+import vsu.tp53.onboardapplication.model.entity.SessionInfoBody
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 const val baseUrl = "http://193.233.49.112"
-class SessionService (
+class SessionService    (
     private val restTemplate: RestTemplate,
     val context: Context
 ) {
@@ -24,27 +35,35 @@ class SessionService (
     private val joinSessionUrl: String = "/join_session/"
     private val leaveSessionUrl: String = "/leave_session/"
 
-    suspend fun getSessions(): Array<SessionBody> {
-        return withContext(Dispatchers.IO) {
-            val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
-            StrictMode.setThreadPolicy(policy)
+    init {
+        val objectMapper = ObjectMapper()
+        val javaTimeModule = JavaTimeModule()
+        val dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")
+        javaTimeModule.addDeserializer(LocalDateTime::class.java, LocalDateTimeDeserializer(dateTimeFormatter))
+        objectMapper.registerModule(javaTimeModule)
 
-            restTemplate.messageConverters.add(MappingJacksonHttpMessageConverter())
-            restTemplate.messageConverters.add(MappingJackson2HttpMessageConverter())
-            restTemplate.messageConverters.add(GsonHttpMessageConverter())
-
-            val url = baseUrl + sessionsUrl;
-            val sessionList = restTemplate.getForObject(
-                url,
-                //тип должен быть сущностью, с json properties, чтобы корректно парситься, насколько я поняла
-                Array<SessionBody>::class.java
-            )
-
-            sessionList
-        }
+        val converter = MappingJackson2HttpMessageConverter()
+        converter.objectMapper = objectMapper
+        restTemplate.messageConverters.add(converter)
     }
 
-    suspend fun getSessionInfo(sessionId : Int): SessionBody? {
+        suspend fun getSessions(): List<SessionBody> {
+            return withContext(Dispatchers.IO) {
+                val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+                StrictMode.setThreadPolicy(policy)
+
+                val url = baseUrl + sessionsUrl;
+                val sessionList: ResponseEntity<Array<SessionBody>> = restTemplate.getForEntity(
+                    url,
+                    //тип должен быть сущностью, с json properties, чтобы корректно парситься, насколько я поняла
+                    Array<SessionBody>::class.java
+                )
+
+                sessionList.body!!.toList()
+            }
+        }
+
+    suspend fun getSessionInfo(sessionId : Int): SessionInfoBody? {
         return withContext(Dispatchers.IO) {
             val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
             StrictMode.setThreadPolicy(policy)
@@ -54,16 +73,11 @@ class SessionService (
             restTemplate.messageConverters.add(GsonHttpMessageConverter())
 
             val url = "$baseUrl$sessionInfoUrl$sessionId";
-            val sessionInfo: SessionBody? = null
-            try {
-                val sessionInfo = restTemplate.getForObject(
-                    url,
-                    //то же самое с типом
-                    SessionBody::class.java
-                )
-            } catch (e: Error) {
-                Log.i("Sessions", "65")
-            }
+            val sessionInfo = restTemplate.getForObject(
+                url,
+                //то же самое с типом
+                SessionInfoBody::class.java
+            )
 
             sessionInfo
         }
