370312abb2adc7df574fdb5f150e0a2973f92ed3
Владислав <yefremov.vlad@gmail.com>
2023-06-12 19:18:30 +0300

    ORG-11: Changed file hierarchy.

--- server/run.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-
-python ./manage.py collectstatic --noinput
-gunicorn --forwarded-allow-ips=* --bind 0.0.0.0:8080 -w 2 onboardProject.wsgi:application
--- server/utils/serializing.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.exceptions import ValidationError
-
-
-def complete_serialize(used_data: dict, serializer):
-    serializing_object = serializer(data=used_data)
-    if not serializing_object.is_valid():
-        raise ValidationError
-    return serializing_object.save()
